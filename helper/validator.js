cf77bc5b70bb9e001e0dafec4cd5abd46f051a1b
aldiwira <aldiwira.aw@gmail.com>
2020-07-13 23:28:15 +0700

    add userhandling

--- helper/validator.js
+++ helper/validator.js
@@ -4,14 +4,20 @@ module.exports = {
   LoginValidator: () => {
     return yup.object().shape({
       username: yup.string().trim().required(),
-      password: yup.string().trim().required(),
+      password: yup.string().trim().min(7).required(),
     });
   },
   RegisterValidator: () => {
     return yup.object().shape({
       username: yup.string().trim().required(),
-      password: yup.string().trim().required(),
+      password: yup.string().trim().min(7).required(),
       name: yup.string().trim().required(),
+    });
+  },
+  changePassword: () => {
+    return yup.object().shape({
+      oldpassword: yup.string().trim().min(7).required(),
+      newpassword: yup.string().trim().min(7).required(),
     });
   },
 };
--- routes/UserHandling.js
+++ routes/UserHandling.js
@@ -4,23 +4,105 @@ const validator = require("../helper/validator");
 const db = require("../helper/db");
 const response = require("../helper/response");
 const encrypt = require("../helper/encrypt");
+const { auth, sign } = require("../helper/jwt");
 
 const router = express.Router();
 const userM = db.get("users");
+
+const existing = async (conditon) => {
+  return await userM.findOne(conditon);
+};
+
+router.get("/user/", auth, async (req, res, next) => {
+  let { _id } = req.payload;
+  try {
+    const userdata = await userM.findOne(_id);
+    if (userdata) {
+      res
+        .status(200)
+        .json(response.set(200, "Success fetch user data", userdata));
+    } else {
+      throw new Error("user not found");
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/user/changeinfo", auth, async (req, res, next) => {
+  let { username, name } = req.body;
+  let { _id } = req.payload;
+  try {
+    const exist = await existing({ $or: [{ _id: _id }] });
+    if (exist) {
+      await userM
+        .findOneAndUpdate(
+          { _id: _id },
+          { $set: { username: username, name: name } }
+        )
+        .then((value) => {
+          res
+            .status(200)
+            .json(response.set(200, "Your account information changed", value));
+        });
+    } else {
+      throw new Error("Account not found");
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/user/changepassword", auth, async (req, res, next) => {
+  let { oldpassword, newpassword } = req.body;
+  let { _id } = req.payload;
+  try {
+    await validator.changePassword().validate({ oldpassword, newpassword });
+    const exist = await existing({
+      $or: [{ _id: _id }],
+    });
+    if (exist) {
+      const checkpassword = await encrypt.auth(exist.password, oldpassword);
+      if (checkpassword) {
+        const newpass = await encrypt.sign(newpassword);
+        await userM
+          .findOneAndUpdate({ _id: _id }, { $set: { password: newpass } })
+          .then((value) => {
+            res
+              .status(200)
+              .json(response.set(200, `Success change accout password`, value));
+          });
+      } else {
+        throw new Error("Wrong password account");
+      }
+    } else {
+      throw new Error("User not found");
+    }
+  } catch (error) {
+    next(error);
+  }
+});
 
 router.post("/login", async (req, res, next) => {
   let { username, password } = req.body;
   try {
     await validator.LoginValidator().validate({ username, password });
-    const existing = await userM.findOne({
+    const exist = await existing({
       $or: [{ username: username }],
     });
-    const checkpassword = await encrypt.auth(existing.password, password);
-    if (existing) {
+    console.log(exist);
+    if (exist) {
+      const checkpassword = await encrypt.auth(exist.password, password);
+      const apitoken = await sign(exist._id);
       if (checkpassword) {
-        res.status(200).json(response.set(200, "Success login", existing));
+        res.status(200).json(
+          response.set(200, "Success login", {
+            user: exist,
+            token: apitoken,
+          })
+        );
       } else {
-        throw new Error("Wrong password");
+        throw new Error("Wrong password account");
       }
     } else {
       throw new Error("Your account not found");
@@ -36,17 +118,23 @@ router.post("/register", async (req, res, next) => {
   const cryptedPassword = await encrypt.sign(password);
   try {
     await validator.RegisterValidator().validate({ username, password, name });
-    const existing = await userM.findOne({
+    const exist = await existing({
       $or: [{ username: username }, { name: name }],
     });
-    if (!existing) {
+    if (!exist) {
       const userD = await userM.insert({
         username,
         password: cryptedPassword,
         name,
         createdAt,
       });
-      res.status(200).json(response.set(200, "Your account created", userD));
+      const apitoken = await sign(userD._id);
+      res.status(200).json(
+        response.set(200, "Your account created", {
+          user: userD,
+          token: apitoken,
+        })
+      );
     } else {
       throw new Error("Your account was available");
     }
